042253afc4748c8cc93ff59d301b32a859485c95
ShengGe <littlejixing@163.com>
2016-07-24 19:36:16 +0800

    review whether the methed is already there before add method

--- basicTools.js
+++ basicTools.js
@@ -2,7 +2,9 @@
 * function: Do not have to type 'prototype' characters when adding method for object.
 */
 Function.prototype.method = function(name, func){
-	this.prototype[name] = func;
+	if (!this.prototype[name]) {
+		this.prototype[name] = func;
+	}
 	return this;
 }
 
