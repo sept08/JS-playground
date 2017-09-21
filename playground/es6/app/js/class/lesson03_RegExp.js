5f7f2182eedae4320c85e716f79e3c68e4b3546c
Tian <littlejixing@163.com>
2017-09-21 15:25:26 +0800

    Update and rename lesson3.js to lesson03_RegExp.js

--- playground/es6/app/js/class/lesson03_RegExp.js
+++ playground/es6/app/js/class/lesson03_RegExp.js
@@ -3,26 +3,23 @@
     let regex = new RegExp('xyz', 'i'); //第一个参数是字符串，第二个是修饰符
     let regex2 = new RegExp(/xyz/i); //第一个参数是正则表达式，不接受第二个参数，否则会报错
     console.log(regex.test('xyz123'), regex2.test('xyz123'));
-    console.log(regex.test('xyZ123'), regex2.test('xyZ123'));
 
     let regex3 = new RegExp(/abc/ig, 'i');
     console.log(regex3.flags); //原有正则对象的修饰符是ig，它会被第二个参数i覆盖
-
 }
 
 // 字符串对象的4个使用正则表达式的方法： match(),replace(),search(),split()这四个方法全部调用RegExp的实例的方法。
-
 {
     let regex = new RegExp('xyz', 'ig');
     console.log(regex.test('xyz0XYZ1xyz2'), regex.exec('xyz0XYZ1xyz2'));
 }
 
 {
-    // y修饰符
+    // y修饰符 黏联模式
     let s = 'bbbb_bbb_bb_b';
     var a1 = /b+/g;
     var a2 = /b+/y;
-
+ 
     console.log(a1.exec(s), a2.exec(s)); // ["bbbb"],["bbbb"]
     console.log(a1.exec(s), a2.exec(s)); // ["bbb"],null
 
@@ -30,6 +27,7 @@
 }
 
 {
+    // Unicode 匹配模式
     console.log('u修饰符',/^\uD83D/.test('\uD83D\uDC2A')); // true
     console.log('u修饰符',/^\uD83D/u.test('\uD83D\uDC2A')); // false
     // 大括号表示Unicode字符，只有加上u才能识别
@@ -47,14 +45,13 @@
     console.log('量词',/𠮷{2}/.test('𠮷𠮷')); // false
     console.log('量词',/𠮷{2}/u.test('𠮷𠮷')); // true
 }
-
+    // ES6中，只是一个提案目前还不支持
 {
     // #正则表达式中，点（.）是一个特殊字符，代表任意的单个字符，但是行终止符（line terminator character）除外
     // U+000A 换行符（\n）
     // U+000D 回车符（\r）
     // U+2028 行分隔符（line separator）
     // U+2029 段分隔符（paragraph separator）
-    // 只是一个提案目前还不支持
     // let reg=/test.go/s;
     // console.log(reg.test('test\ngo'));
     // console.log(reg.test('test\ngo'));
