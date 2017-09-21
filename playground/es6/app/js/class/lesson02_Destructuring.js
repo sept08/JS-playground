aabf2914396b828640faa358710d193c403bcc76
Tian <littlejixing@163.com>
2017-09-21 10:47:16 +0800

    comment
    add comments
    rename filename

--- playground/es6/app/js/class/lesson02_Destructuring.js
+++ playground/es6/app/js/class/lesson02_Destructuring.js
@@ -15,20 +15,20 @@
   ({a,b}={a:1,b:2})
   console.log(a,b);
 }
-
+// 对象没有配对成功，值为undefined
 {
   let a,b,c,rest;
   [a,b,c=3]=[1,2];
   console.log(a,b,c);
 }
-
+// 变量交换
 {
   let a=1;
   let b=2;
   [a,b]=[b,a];
   console.log(a,b);
 }
-
+// 接收处理结果
 {
   function f(){
     return [1,2]
@@ -37,7 +37,7 @@
   [a,b]=f();
   console.log(a,b);
 }
-
+// 数组解构赋值，部分取用
 {
   function f(){
     return [1,2,3,4,5]
@@ -46,7 +46,7 @@
   [a,,,b]=f();
   console.log(a,b);
 }
-
+// 只关心第一个索引值，但不清楚数组长度
 {
   function f(){
     return [1,2,3,4,5]
@@ -55,18 +55,18 @@
   [a,...b]=f();
   console.log(a,b);
 }
-
+// 对象解构赋值，注意接收侧保持对象格式
 {
   let o={p:42,q:true};
   let {p,q}=o;
   console.log(p,q);
 }
-
+// 对象解构赋值，默认值处理
 {
   let {a=10,b=5}={a:3};
   console.log(a,b);
 }
-
+// 模拟处理后端json数据
 {
   let metaData={
     title:'abc',
