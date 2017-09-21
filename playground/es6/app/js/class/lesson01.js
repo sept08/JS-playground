dc906b8913e9d15c425e349128736663b0b826dd
Tian <littlejixing@163.com>
2017-09-21 10:26:53 +0800

    add comments

--- playground/es6/app/js/class/lesson01.js
+++ playground/es6/app/js/class/lesson01.js
@@ -8,11 +8,11 @@ function test(){
 }
 
 function last(){
-  const PI=3.1415926;
+  const PI=3.1415926; // 常量也是块级作用域
   const k={
     a:1
   }
-  k.b=3;
+  k.b=3; // 对象和数组的赋值为引用赋值，引用的指针不可修改，但所引用的对象可以修改
   console.log(PI,k);
 }
 
