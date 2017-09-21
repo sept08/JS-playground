7e6faac7ade7dff3ac019f334baf44157d9637c4
Tian <littlejixing@163.com>
2017-09-21 20:16:01 +0800

    Update and rename lesson4.js to lesson04_Unicode.js

--- playground/es6/app/js/class/lesson04_Unicode.js
+++ playground/es6/app/js/class/lesson04_Unicode.js
@@ -1,39 +1,38 @@
+// unicode
 {
   console.log('a',`\u0061`);
   console.log('s',`\u20BB7`);
 
   console.log('s',`\u{20BB7}`);
-
-
 }
-
 
 {
   let s='𠮷';
-  console.log('length',s.length);
-  console.log('0',s.charAt(0));
+  console.log('length',s.length); // 2
+  console.log('0',s.charAt(0)); // charAt 获取按每两个字节组成的字符(ASCII码)
   console.log('1',s.charAt(1));
-  console.log('at0',s.charCodeAt(0));
+  console.log('at0',s.charCodeAt(0));// charCodeAt 获取两个字节组成字符的ASCII码值
   console.log('at1',s.charCodeAt(1));
 
   let s1='𠮷a';
   console.log('length',s1.length);
-  console.log('code0',s1.codePointAt(0));
+  console.log('code0',s1.codePointAt(0));// codePointAt 获取Unicode码值，传参为识别起始点，单位为字(16位)
   console.log('code0',s1.codePointAt(0).toString(16));
   console.log('code1',s1.codePointAt(1));
   console.log('code2',s1.codePointAt(2));
 }
 
 {
-  console.log(String.fromCharCode("0x20bb7"));
-  console.log(String.fromCodePoint("0x20bb7"));
+  console.log(String.fromCharCode("0x20bb7"));// ES5，码值转化为字符
+  console.log(String.fromCodePoint("0x20bb7"));// ES6，码值转化为字符，可处理Unicode
 }
-
+// 字符串遍历
 {
   let str='\u{20bb7}abc';
   for(let i=0;i<str.length;i++){
     console.log('es5',str[i]);
   }
+  // 可处理包含Unicode字符的字符串
   for(let code of str){
     console.log('es6',code);
   }
