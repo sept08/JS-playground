781e144a883f350c22dd9036315061b0a4759ee8
Tian <littlejixing@163.com>
2017-09-21 21:21:11 +0800

    Update and rename lesson5.js to lesson05_Number.js

--- playground/es6/app/js/class/lesson05_Number.js
+++ playground/es6/app/js/class/lesson05_Number.js
@@ -1,8 +1,9 @@
+// 二进制与八进制
 {
   console.log('B',0B111110111);
   console.log(0o767);
 }
-
+// 判断有限，NaN
 {
   console.log('15',Number.isFinite(15));
   console.log('NaN',Number.isFinite(NaN));
@@ -11,25 +12,25 @@
   console.log('0',Number.isNaN(0));
 
 }
-
+// 判断整数
 {
   console.log('25',Number.isInteger(25));
   console.log('25.0',Number.isInteger(25.0));
   console.log('25.1',Number.isInteger(25.1));
   console.log('25.1',Number.isInteger('25'));
 }
-
+// 准确整数范围
 {
   console.log(Number.MAX_SAFE_INTEGER,Number.MIN_SAFE_INTEGER);
   console.log('10',Number.isSafeInteger(10));
   console.log('a',Number.isSafeInteger('a'));
 }
-
+// 取整数部分
 {
   console.log(4.1,Math.trunc(4.1));
   console.log(4.9,Math.trunc(4.9));
 }
-
+// 判断符号
 {
   console.log('-5',Math.sign(-5));
   console.log('0',Math.sign(0));
@@ -37,8 +38,7 @@
   console.log('50',Math.sign('50'));
   console.log('foo',Math.sign('foo'));
 }
-
-
+// 立方根
 {
   console.log('-1',Math.cbrt(-1));
   console.log('8',Math.cbrt(8));
