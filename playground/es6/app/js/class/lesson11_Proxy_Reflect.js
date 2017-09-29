f9fc41bd91d238d9d81f6b0820682068ad26cbcf
Tian <littlejixing@163.com>
2017-09-29 10:34:37 +0800

    Update and rename lesson11.js to lesson11_Proxy_Reflect.js

--- playground/es6/app/js/class/lesson11_Proxy_Reflect.js
+++ playground/es6/app/js/class/lesson11_Proxy_Reflect.js
@@ -3,7 +3,7 @@
     time:'2017-03-11',
     name:'net',
     _r:123
-  };
+  }
 
   let monitor=new Proxy(obj,{
     // 拦截对象属性的读取
@@ -13,9 +13,9 @@
     // 拦截对象设置属性
     set(target,key,value){
       if(key==='name'){
-        return target[key]=value;
+        return target[key]=value
       }else{
-        return target[key];
+        return target[key]
       }
     },
     // 拦截key in object操作
@@ -23,39 +23,36 @@
       if(key==='name'){
         return target[key]
       }else{
-        return false;
+        return false
       }
     },
     // 拦截delete
     deleteProperty(target,key){
       if(key.indexOf('_')>-1){
-        delete target[key];
+        delete target[key]
         return true;
       }else{
         return target[key]
       }
     },
-    // 拦截Object.keys,Object.getOwnPropertySymbols,Object.getOwnPropertyNames
+    // 拦截Object.keys, Object.getOwnPropertySymbols, Object.getOwnPropertyNames
     ownKeys(target){
       return Object.keys(target).filter(item=>item!='time')
     }
-  });
+  })
 
-  console.log('get',monitor.time);
+  console.log('get',monitor.time)
+  monitor.time='2018'
+  monitor.name='mukewang'
+  console.log('set',monitor.time,monitor)
+  console.log('has','name' in monitor,'time' in monitor)
 
-  monitor.time='2018';
-  monitor.name='mukewang';
-  console.log('set',monitor.time,monitor);
-
-  console.log('has','name' in monitor,'time' in monitor);
-
-  // delete monitor.time;
-  // console.log('delete',monitor);
+  // delete monitor.time
+  // console.log('delete',monitor)
   //
-  // delete monitor._r;
-  // console.log('delete',monitor);
-  console.log('ownKeys',Object.keys(monitor));
-
+  // delete monitor._r
+  // console.log('delete',monitor)
+  console.log('ownKeys',Object.keys(monitor))
 }
 
 {
@@ -78,7 +75,7 @@
       _validator:validator,
       set(target,key,value,proxy){
         if(target.hasOwnProperty(key)){
-          let va=this._validator[key];
+          let va=this._validator[key]
           if(!!va(value)){
             return Reflect.set(target,key,value,proxy)
           }else{
@@ -105,18 +102,15 @@
 
   class Person{
     constructor(name,age){
-      this.name=name;
-      this.age=age;
-      this.mobile='1111';
+      this.name=name
+      this.age=age
+      this.mobile='1111'
       return validator(this,personValidators)
     }
   }
 
-  const person=new Person('lilei',30);
-
-  console.info(person);
-
-  person.name='Han mei mei';
-
-  console.info(person);
+  const person=new Person('lilei',30)
+  console.info(person)
+  person.name='Han mei mei'
+  console.info(person)
 }
