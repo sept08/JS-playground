9a63983bd3fa85a37d6aa12cd0b62cd20ee7778b
Tian <littlejixing@163.com>
2017-09-30 23:10:37 +0800

    class of es6

--- playground/es6/app/js/class/lesson12_Class.js
+++ playground/es6/app/js/class/lesson12_Class.js
@@ -2,18 +2,18 @@
   // 基本定义和生成实例
   class Parent{
     constructor(name='mukewang'){
-      this.name=name;
+      this.name=name
     }
   }
-  let v_parent=new Parent('v');
-  console.log('构造函数和实例',v_parent);
+  let v_parent=new Parent('v')
+  console.log('构造函数和实例',v_parent)
 }
 
 {
   // 继承
   class Parent{
     constructor(name='mukewang'){
-      this.name=name;
+      this.name=name
     }
   }
 
@@ -21,32 +21,32 @@
 
   }
 
-  console.log('继承',new Child());
+  console.log('继承',new Child())
 }
 
 {
   // 继承传递参数
   class Parent{
     constructor(name='mukewang'){
-      this.name=name;
+      this.name=name
     }
   }
 
   class Child extends Parent{
     constructor(name='child'){
-      super(name);
-      this.type='child';
+      super(name)
+      this.type='child'
     }
   }
 
-  console.log('继承传递参数',new Child('hello'));
+  console.log('继承传递参数',new Child('hello'))
 }
 
 {
   // getter,setter
   class Parent{
     constructor(name='mukewang'){
-      this.name=name;
+      this.name=name
     }
 
     get longName(){
@@ -54,14 +54,14 @@
     }
 
     set longName(value){
-      this.name=value;
+      this.name=value
     }
   }
 
-  let v=new Parent();
-  console.log('getter',v.longName);
-  v.longName='hello';
-  console.log('setter',v.longName);
+  let v=new Parent()
+  console.log('getter',v.longName)
+  v.longName='hello'
+  console.log('setter',v.longName)
 }
 
 {
@@ -72,29 +72,26 @@
     }
 
     static tell(){
-      console.log('tell');
+      console.log('tell')
     }
   }
 
-  Parent.tell();
-
+  Parent.tell()
 }
 
 {
   // 静态属性
   class Parent{
     constructor(name='mukewang'){
-      this.name=name;
+      this.name=name
     }
 
     static tell(){
-      console.log('tell');
+      console.log('tell')
     }
   }
+  // 在类定义完后直接通过类名定义
+  Parent.type='test'
 
-  Parent.type='test';
-
-  console.log('静态属性',Parent.type);
-
-
+  console.log('静态属性',Parent.type)
 }
