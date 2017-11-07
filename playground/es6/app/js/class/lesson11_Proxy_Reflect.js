6bed046a202af1e3867c38bc400a26d74f57a32a
Tian <littlejixing@163.com>
2017-11-07 10:18:32 +0800

    Update lesson11_Proxy_Reflect.js

--- playground/es6/app/js/class/lesson11_Proxy_Reflect.js
+++ playground/es6/app/js/class/lesson11_Proxy_Reflect.js
@@ -86,6 +86,14 @@
 }
 
 {
+  // Reflect
+  /*
+  用法
+  1. 将Object对象的一些明显属于语言内部的方法，放到Reflect对象上。
+  2. 修改某些Object方法的返回结果，让其变得更合理。
+  3. 让Object操作都变成函数行为。
+  4. Reflect对象的方法与Proxy对象的方法一一对应，只要是Proxy对象的方法，就能在Reflect对象上找到对应的方法。
+  */
   let obj={
     time:'2017-03-11',
     name:'net',
