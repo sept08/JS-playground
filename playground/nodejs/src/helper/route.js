8be5ce79a05601947cf598985b735da99671df47
ShengGe <littlejixing@163.com>
2017-11-25 20:57:46 +0800

    update code

--- playground/nodejs/src/helper/route.js
+++ playground/nodejs/src/helper/route.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
+const path = require('path')
+const Handlebars = require('handlebars')
 const { promisify } = require('util')
+const conf = require('../config/defaultConfig')
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
+// __dirname 当前文件的绝对路径
+const tplPath = path.join(__dirname, '../template/dir.html')
+const source = fs.readFileSync(tplPath)
+const template = Handlebars.compile(source.toString())
 
 module.exports = async function (req, res, filePath) {
     try {
@@ -13,8 +20,14 @@ module.exports = async function (req, res, filePath) {
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath)
             res.statusCode = 200
-            res.setHeader('Content-Type', 'text/plain')
-            res.end(files.join('\n'))
+            res.setHeader('Content-Type', 'text/html')
+            const dir = path.relative(conf.root, filePath)
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files
+            }
+            res.end(template(data))
         }
     } catch (error) {
         res.statusCode = 404
