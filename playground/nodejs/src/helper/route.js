195730599e52dd0027bba7943f16ec8da2b48512
ShengGe <littlejixing@163.com>
2017-11-26 11:56:59 +0800

    add cache function

--- playground/nodejs/src/helper/route.js
+++ playground/nodejs/src/helper/route.js
@@ -3,6 +3,10 @@ const path = require('path')
 const Handlebars = require('handlebars')
 const { promisify } = require('util')
 const conf = require('../config/defaultConfig')
+const mime = require('./mime')
+const compress = require('./compress')
+const isFresh = require('./cache')
+
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
 // __dirname 当前文件的绝对路径
@@ -14,9 +18,21 @@ module.exports = async function (req, res, filePath) {
     try {
         const stats = await stat(filePath)
         if (stats.isFile()) {
+            const contentType = mime(filePath)
+            res.setHeader('Content-Type', contentType)
+
+            if(isFresh(stats, req, res)){
+                res.statusCode = 304
+                res.end()
+                return
+            }
+
             res.statusCode = 200
-            res.setHeader('Content-Type', 'text/plain')
-            fs.createReadStream(filePath).pipe(res)
+            let rs = fs.createReadStream(filePath)
+            if(filePath.match(conf.compress)){
+                rs = compress(rs, req, res)
+            }
+            rs.pipe(res)
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath)
             res.statusCode = 200
